11c3148a722a23b3862cd9038439d7d486808c4e
Muliminty <313195303@qq.com>
2024-10-25 23:58:13 +0800

    feat(env): 新增环境变量配置

--- env.js
+++ env.js
@@ -5,6 +5,7 @@
  * @property {string} OTHER_VARIABLE - 其他变量
  * @property {string} VITE_NAME - 环境名称
  * @property {string} VITE_APP_ENV - 应用环境标识
+ * @property {Object} proxy - 代理配置
  */
 
 /**
@@ -18,22 +19,45 @@ const env = {
         VITE_PORT: 3000,
         OTHER_VARIABLE: 'value_for_dev',
         VITE_NAME: 'dev环境',
-        VITE_APP_ENV: 'dev'
+        VITE_APP_ENV: 'dev',
+        proxy: {
+            '/api': {
+                target: 'https://dev.api.yourservice.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
     },
     staging: {
         VITE_API_BASE_URL: 'https://staging.api.yourservice.com',
         VITE_PORT: 3001,
         OTHER_VARIABLE: 'value_for_staging',
         VITE_NAME: 'staging环境',
-        VITE_APP_ENV: 'staging'
+        VITE_APP_ENV: 'staging',
+        proxy: {
+            '/api': {
+                target: 'https://staging.api.yourservice.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
     },
     production: {
         VITE_API_BASE_URL: 'https://api.yourservice.com',
         VITE_PORT: 3002,
         OTHER_VARIABLE: 'value_for_production',
         VITE_NAME: 'prod环境',
-        VITE_APP_ENV: 'prod'
-    }
+        VITE_APP_ENV: 'prod',
+        proxy: {
+            '/api': {
+                target: 'https://api.yourservice.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
+    },
+
+    // Add more environments as needed
 };
 
-export default env
+export default env;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'
+import envConfig from './env';
+// import autoImportStyles from './plugin/vite-plugin-auto-import-styles';
+// import viteCustomStartupInfo from './plugin/vite-custom-startup-info'; // 导入自定义插件
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = envConfig[mode];
+
+  return {
+    plugins: [
+      react(),
+      // autoImportStyles(),
+      // viteCustomStartupInfo()
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(env.VITE_PORT, 10),
+      proxy: env.proxy // 传入代理配置
+    },
+    presets: ["@babel/preset-react"],
+    define: {
+      'process.env': env
+    },
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/, // 正则表达式来匹配 .js 和 .jsx 文件
+    },
+    resolve: {
+      alias: {
+        // eslint-disable-next-line no-undef
+        "@": resolve(__dirname, 'src'), // 路径别名
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/scss/variables.scss";
+            @import "@/assets/scss/utils.scss";
+          `
+        }
+      }
+    }
+  };
+});
